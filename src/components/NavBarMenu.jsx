bc44ee9079eddf9af1d7aa1ba260579598d7926e
Facundo Adaro <adarbox@gmail.com>
2023-04-17 01:23:08 -0300

    feat:add navbar menu

---
+++ src/components/NavBarMenu.jsx
@@ -0,0 +1,26 @@
+import { Container, Navbar } from "react-bootstrap"
+
+function NavBarMenu() {
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#!">
+                        Dynamic <b>Form</b>
+                    </Navbar.Brand>
+                    <Navbar.Toggle>
+
+                    </Navbar.Toggle>
+                    <Navbar.Collapse className="justify-content-end">
+                        <Navbar.Text>
+                            Signed in as:{" "}
+                            <a href="www.instagram.com/facuu.adaro" target="blank" rel="noreferrer">Facu Adaro</a>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
+
+export default NavBarMenu
